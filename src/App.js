048280dd5874ffd0b7d7bc24e22ae2710843fe73
HotsumaX <luislaborde@gmail.com>
2019-05-27 15:38:51 -0400

    completed the first attempt of building of the todo list app, archiving files to reattempt

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useRef, useState } from 'react';
+import TodoList from './TodoList';
+import TodoItems from './TodoItems';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [items, setItems] = useState([]);
+  const [currentItem, setCurrentItem] = useState({ text: '', key: '' });
+  const inputElement = useRef(null);
+
+  const handleInput = e => {
+    const itemText = e.target.value;
+    const currentItem = { text: itemText, key: Date.now() };
+    setCurrentItem(currentItem);
+    console.log('hello input');
+  };
+
+  const addItem = e => {
+    e.preventDefault();
+    const newItem = currentItem;
+    if (newItem.text !== '') {
+      console.log(newItem);
+      const newItems = [...items, newItem];
+      setItems(newItems);
+      setCurrentItem({ text: '', key: '' });
+    }
+  };
+  const deleteItem = key => {
+    const filteredItems = items.filter(item => {
+      return item.key !== key;
+    });
+    setItems(filteredItems);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TodoList
+        addItem={addItem}
+        inputElement={inputElement}
+        handleInput={handleInput}
+        currentItem={currentItem}
+      />
+      <TodoItems entries={items} deleteItem={deleteItem} />
     </div>
   );
-}
+};
 
 export default App;
