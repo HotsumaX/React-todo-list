3a31549d2f27cbe5ee5304076d6f4b57fa404613
HotsumaX <luislaborde@gmail.com>
2019-05-31 11:10:33 -0400

    refactored todolist and todoitem files

--- src/TodoItems.js
+++ src/TodoItems.js
@@ -1,18 +1,16 @@
 import React from 'react';
 
-class TodoItems extends React.Component {
-  createTasks = item => {
+const TodoItems = props => {
+  const createTasks = item => {
     return (
-      <li key={item.key} onClick={() => this.props.deleteItem(item.key)}>
+      <li key={item.key} onClick={() => props.deleteItem(item.key)}>
         {item.text}
       </li>
     );
   };
+  const todoEntries = props.entries;
+  const listItems = todoEntries.map(createTasks);
 
-  render() {
-    const todoEntries = this.props.entries;
-    const listItems = todoEntries.map(this.createTasks);
-    return <ul className="theList">{listItems}</ul>;
-  }
-}
+  return <ul className="theList">{listItems}</ul>;
+};
 export default TodoItems;
