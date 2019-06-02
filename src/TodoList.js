894028fda4add8ca4155b66bd5fc9a99c1b420a0
HotsumaX <luislaborde@gmail.com>
2019-06-02 09:06:10 -0400

    finished the importing of props to the todoList component

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
-const TodoList = ({ addItem }) => {
+const TodoList = ({ addItem, inputElement, currentItem, handleInput }) => {
+  useEffect(() => {
+    inputElement.current.focus();
+  });
+
   return (
     <div className="todoListMain">
       <div className="header">
         <form onSubmit={addItem}>
-          <input placeholder="Task" />
+          <input
+            placeholder="Task"
+            ref={inputElement}
+            value={currentItem.text}
+            onChange={handleInput}
+          />
           <button type="submit">Add Task</button>
         </form>
       </div>
